1f3b2e8f6b3751ffb6320ed64bef7ce91d9fa0d6
Genevieve <genevieve.point@my.jcu.edu.au>
2016-03-27 23:31:34 +1000

    4th workshop albeit not completed yet Also BlacksASCII_lesson does not work at the moment

--- BlaccksASCII_Lesson.py
+++ BlaccksASCII_Lesson.py
@@ -1,23 +1,43 @@
-# lower = 10
-# upper = 100
+LOWER_VALUE = 31
+UPPER_VALUE = 176
 # print("Enter a number (" + {} + "-" + {} + "):".format(lower, upper))
-#strip()
+# strip()
 
-lower_value = int(input("Please enter an ASCII value: ").strip())
-while lower_value < 10 and lower_value >50:
-   print("The lowest possible value is 10")
-   lower_value = int(input("Please enter an ASCII value: ").strip())
-print("Thank You")
-
-upper_value = int(input("Please enter an ASCII value: ").strip())
-while upper_value <10 and upper_value >50:
-   print("The highest possible value is 50")
-   upper_value = int(input("Please enter an ASCII value: ").strip())
-print("Thank You")
-
-for i in range(10, 120, 11):
-    # make sure we have integers of different 'length'
-    print("{} {}".format(i, chr(i)))
 
 def get_number(lower_value, upper_value):
-    
+
+    lower_value = input("Enter the starting ASCII value: ").strip()
+
+    while lower_value.isdecimal() == False:
+        print("Please enter a decimal number")
+        lower_value = input("Enter the starting ASCII value: ").strip()
+    while lower_value < LOWER_VALUE:
+        print("The lowest possible value is 10")
+        lower_value = input("Please enter an ASCII value: ").strip()
+    print("Thank You")
+
+    upper_value = input("Enter the final ASCII value: ").strip()
+
+    while upper_value.isdecimal() == False:
+        print("Please enter")
+        upper_value = int(input("Please enter an ASCII value: ").strip())
+    while upper_value < UPPER_VALUE:
+        print("The highest possible value is 50")
+        upper_value = int(input("Please enter an ASCII value: ").strip())
+    print("Thank You")
+
+    integer_lower_value = int(lower_value)
+    integer_upper_value = int(upper_value)
+
+    return (integer_lower_value, integer_upper_value)
+
+ASCII_values = get_number(LOWER_VALUE,UPPER_VALUE)
+
+print("Displaying ASCII Value between \n (()-()) :".format(ASCII_values(0), ASCII_values(1)))
+
+# for i in range(10, 120, 11):
+#     make sure we have integers of different 'length'
+#     print("{} {}".format(i, chr(i)))
+
+
+
